d0b6f7e13c9e50ceae0e1b5acc944affcc908502
Earl Brian Baclohan <earlbrianbaclohan1201@gmail.com>
2025-09-27 13:55:39 +0800

    Chapter 7: Refactor job routes to a resourceful controller

--- routes/web.php
+++ routes/web.php
@@ -1,65 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
+use App\Http\Controllers\JobController;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () {
-    return view('jobs.index', [
-        'jobs' => \App\Models\Job::with('employer')->paginate(10)
-    ]);
-});
-
-// Show Create Form (MUST be before /jobs/{job})
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-// Store a New Job
-Route::post('/jobs', function () {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    \App\Models\Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-
-    return redirect('/jobs');
-});
-
-Route::get('/jobs/{job}', function (\App\Models\Job $job) {
-    return view('jobs.show', ['job' => $job]);
-});
-
-// Show Edit Form
-Route::get('/jobs/{job}/edit', function (\App\Models\Job $job) {
-    return view('jobs.edit', ['job' => $job]);
-});
-
-// Update a Job
-Route::patch('/jobs/{job}', function (\App\Models\Job $job) {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary'),
-    ]);
-
-    return redirect('/jobs/' . $job->id);
-});
-
-// Destroy a Job
-Route::delete('/jobs/{job}', function (\App\Models\Job $job) {
-    $job->delete();
-    return redirect('/jobs');
-});
+// Using resourceful routing (this replaces all 7 individual routes)
+Route::resource('jobs', JobController::class);
